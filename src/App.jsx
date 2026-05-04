77a92730963657f28e9c957951cf01ef3e323bd0
angelleguia <angelochek9019@gmail.com>
2026-05-04 18:13:33 -0400

    App modified, Profiles modified.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import Card from './components/Card.jsx'
+import Card from './components/Card.jsx';
 import { profiles } from './profiles.js';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [profiles, setProfiles] = useState([]);
+
+  useEffect(() => {
+    const fetchProfiles = async () => {
+      try {
+        const response = await fetch('http://localhost:4000/api/profiles')
+        //  .then(res => res.json())
+        //  .then(data => console.log(data))
+
+        const data = await response.json();
+
+        setProfiles(data.profiles)
+      } catch (error) {
+        console.error("Error fetching profiles:", error);
+      }
+    }
+
+    fetchProfiles();
+  }, [])
+
 
   return (
     <div className="flex-container">
       {profiles.map((profile) => (
-        <Card key={profile.id} name={profile.name} title={profile.title} bio={profile.bio} />
+        <Card key={profile._id} name={profile.name} title={profile.title} bio={profile.bio} />
       ))}
     </div>
-
   );
 }
 
--- src/profiles.js
+++ src/profiles.js
@@ -1,15 +1,14 @@
 export const profiles = [
-    {
-        id:1,
-        name:"Lange",
-        title:"Full-Stack Developer",
-        bio:"I am ready to rock as a Full-Stacker!"
-    },
-    {
-        id:2,
-        name:"Diogo",
-        title:"Back-End Developer",
-        bio:"I am ready to rumble with the Back-End!"
-    }
-]
-
+  {
+    id: 1,
+    name: "Lange",
+    title: "Full-Stack Developer",
+    bio: "I am ready to rock as a Full-Stacker!",
+  },
+  {
+    id: 2,
+    name: "Diogo",
+    title: "Back-End Developer",
+    bio: "I am ready to rumble with the Back-End!",
+  },
+];
